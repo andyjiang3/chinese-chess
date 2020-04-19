3b367936bf42cb10db5ade6005aa72b7d6724318
andy jiang <andyjiang55@yahoo.com>
2020-04-19 00:21:01 -0400

    added constructor for piece and captured piece, getter method to get them

--- GameLogic/Move.java
+++ GameLogic/Move.java
@@ -13,6 +13,9 @@ import GameLogic.Pieces.Piece;
  */
 
 public class Move {
+    private Piece piece;
+    private Piece capturedPiece;
+
     private int originX;
     private int originY;
     private int finalX;
@@ -50,6 +53,45 @@ public class Move {
 
     public Move(Piece piece, int originX, int originY, int finalX, int finalY) {
         //this iteration makes the move object hold the piece? Not sure how to structure
+        this.piece = piece;
+        this.originX = originX;
+        this.originY = originY;
+        this.finalX = finalX;
+        this.finalY = finalY;
+
+        this.dx = finalX - originX;
+        this.dy = finalY - originY;
+        if (dx == 0 && dy != 0) {
+            this.isVertical = true;
+        }
+        if (dy == 0 && dx != 0) {
+            this.isHorizontal = true;
+        }
+        if (Math.abs(dx) == Math.abs(dy) && dx != 0) {
+            this.isDiagonal = true;
+        }
+    }
+
+    public Move(Piece piece, Piece capturedPiece, int originX, int originY, int finalX, int finalY) {
+
+        this.piece = piece;
+        this.capturedPiece = capturedPiece;
+        this.originX = originX;
+        this.originY = originY;
+        this.finalX = finalX;
+        this.finalY = finalY;
+
+        this.dx = finalX - originX;
+        this.dy = finalY - originY;
+        if (dx == 0 && dy != 0) {
+            this.isVertical = true;
+        }
+        if (dy == 0 && dx != 0) {
+            this.isHorizontal = true;
+        }
+        if (Math.abs(dx) == Math.abs(dy) && dx != 0) {
+            this.isDiagonal = true;
+        }
     }
 
     public void setValid(boolean v) {
@@ -94,6 +136,14 @@ public class Move {
 
     public boolean isValid() {
         return isValid;
+    }
+
+    public Piece getPiece() {
+        return piece;
+    }
+
+    public Piece getCapturedPiece() {
+        return capturedPiece;
     }
 
 
