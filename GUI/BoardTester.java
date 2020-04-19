d74d1aa9d6b2cbd1f78f14100fbf55737d9178b6
MichaelYu15 <52168207+MichaelYu15@users.noreply.github.com>
2020-04-19 01:08:33 -0400

    Add files via upload
    added a gui with game board

---
+++ GUI/BoardTester.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+public class BoardTester {
+	public static void main(String[] args) {
+		BoardFrame border = new BoardFrame(); 
+	    border.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+	    border.setSize( 700, 700 ); // set frame size
+	    border.setVisible( true );
+	}
+}
