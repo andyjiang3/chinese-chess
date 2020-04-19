e5025f9ea5595edc349fd4ccb33c7f716167ef6e
andy jiang <andyjiang55@yahoo.com>
2020-04-19 00:21:51 -0400

    added movelogger

--- GameLogic/tester.java
+++ GameLogic/tester.java
@@ -11,6 +11,7 @@ public class tester {
 
 
 
+
         //test player side methods
         Player player1 = new Player(1, "Hi", Piece.Side.DOWN);
         Player player2 = new Player(2, "Hi2", Piece.Side.UP );
@@ -21,6 +22,35 @@ public class tester {
 
 
         }
+
+        //test new Move methods/constructor
+        Move move1 = new Move(new Horse(player1.getPlayerSide()),4, 8, 4, 9);
+        Move move2 = new Move(new Cannon(player1.getPlayerSide()), new Soldier(player1.getPlayerSide()), 3, 2,1, 2);
+        Move move3 = new Move(new Horse(player1.getPlayerSide()),4, 8, 4, 9);
+        Move move4 = new Move(new Cannon(player1.getPlayerSide()), new Soldier(player1.getPlayerSide()), 3, 2,1, 2);
+        Move move5 = new Move(new Horse(player1.getPlayerSide()),4, 8, 4, 9);
+        Move move6 = new Move(new Cannon(player1.getPlayerSide()),4, 8, 4, 9);
+        Move move7 = new Move(new Cannon(player1.getPlayerSide()), new Soldier(player1.getPlayerSide()), 3, 2,1, 2);
+        if (move1.getCapturedPiece() == null) {
+            System.out.println("Hey");
+        }
+        System.out.println(move2.getCapturedPiece());
+
+        //test MoveLogger\
+//        MoveLogger logger = new MoveLogger();
+//        logger.addMove(move1);
+//        logger.addMove(move2);
+//        logger.addMove(move3);
+//        logger.addMove(move4);
+//        logger.addMove(move5);
+//        logger.addMove(move6);
+//        logger.addMove(move7);
+//        logger.printAllMoves(player1);
+
+
+
+
+
 
         //test color
         System.out.println("Color: " + player1.getColor());
@@ -43,6 +73,7 @@ public class tester {
 
         Board gBoard1 = new Board();
         Scanner in = new Scanner(System.in);
+        MoveLogger logger = new MoveLogger();
 
         int x, y, finalX, finalY;
 
@@ -51,38 +82,64 @@ public class tester {
         while(true) {
 
             if (counter % 2 == 0) {
-                System.out.println(player1.getName() + "'s turn. " + "(" + player1.getColor() + " Side)" + " ===============");
+
+                System.out.println(player1.getName() + "'s turn. " + "(" + player1.getColor() + " Side)" + " - Round " + logger.getRound() + " ===============");
                 System.out.print("Total Pieces Captured: " + player1.getNumPiecesCaptured() + " | ");
                 player1.printElapsedTime();
+                System.out.println();
+                System.out.println("Move Logger: ");
+                logger.printAllMoves(player1);
+                System.out.println();
+
                 System.out.println("Move (x y x y): ");
                 player1.startTurnTimer();
                 x = in.nextInt();
                 y = in.nextInt();
                 finalX = in.nextInt();
                 finalY = in.nextInt();
-                player1.tryMove(new Move(x, y, finalX, finalY), gBoard1);
+                player1.tryMove(new Move(x, y, finalX, finalY), gBoard1, logger);
                 player1.stopTurnTimer();
+
             }
 
             if (counter % 2 == 1) {
-                System.out.println(player2.getName() + "'s turn. " + "(" + player2.getColor() + " Side)" + " ===============");
+                System.out.println(player2.getName() + "'s turn. " + "(" + player2.getColor() + " Side)" + " - Round " + logger.getRound() + " ===============");
                 System.out.print("Total Pieces Captured: " + player2.getNumPiecesCaptured() + " | ");
                 player2.printElapsedTime();
+                System.out.println();
+                System.out.println("Move Logger: ");
+                logger.printAllMoves(player2);
+                System.out.println();
+
                 System.out.println("Move (x y x y): ");
                 player2.startTurnTimer();
                 x = in.nextInt();
                 y = in.nextInt();
                 finalX = in.nextInt();
                 finalY = in.nextInt();
-                player2.tryMove(new Move(x, y, finalX, finalY), gBoard1);
+                player2.tryMove(new Move(x, y, finalX, finalY), gBoard1, logger);
                 player2.stopTurnTimer();
-        }
+
+            }
+
+
 
             gBoard1.printBoard();
             System.out.println("############################################################################################################################");
             System.out.println();
             counter++;
         }
+        /*
+               Red: 4 9 4 8 General
+               Black: 4 3 4 4 Soldier
+               Red: 4 6 4 5 Soldier
+               Black: 4 4 4 5 Soldier! Captured Soldier
+               Red: 0 6 0 5 Soldier
+               Black 4 5 4 6 Soldier
+               Red: 7 9 8 7 Horse
+               Black: 8 3 8 4 Soldier
+               Red:
+         */
 
 
     }
