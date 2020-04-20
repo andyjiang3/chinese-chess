1e582cc4ccbd8cf667be83d9596c75b822d84574
andy jiang <andyjiang55@yahoo.com>
2020-04-20 01:42:27 -0400

    changed size

--- GUI/BoardTester.java
+++ GUI/BoardTester.java
@@ -8,7 +8,7 @@ public class BoardTester {
 	public static void main(String[] args) {
 		BoardFrame border = new BoardFrame(); 
 	    border.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-	    border.setSize( 700, 700 ); // set frame size
+	    border.setSize( 900, 700 ); // set frame size
 	    border.setVisible( true );
 	}
 }
