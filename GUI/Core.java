fd06e039120a83c04fc26e38d93a5dc49753b6ed
Venkat Pamulapati <venkatratnamcp@gmail.com>
2020-04-20 15:10:08 -0400

    Kinda lets everything work, player switching is mega broken but you can see stuff in action.

--- GUI/Core.java
+++ GUI/Core.java
@@ -4,6 +4,8 @@ import javax.swing.JFrame;
 
 import GameLogic.Board;
 import GameLogic.Move;
+import GameLogic.Pieces.Piece;
+import GameLogic.Player;
 
 public class Core {
 	
@@ -12,11 +14,18 @@ public class Core {
     private BoardFrame boardFrame;
     private BoardPanel boardPanel;
     private Board board;
+    private int counter;
+    private Player player1;
+    private Player player2;
     
 	public Core() {
 		board = new Board();
 		boardPanel = new BoardPanel(this);
 		boardFrame = new BoardFrame(this);
+        player1 = new Player(1, "DownGamer", Piece.Side.DOWN);
+        player2 = new Player(2, "GamersRise", Piece.Side.UP);
+        counter = 0;
+
 		//board.tryMove(new Move(4, 9, 4, 8 ));
 		//boardPanel.userRepaint();
     }
@@ -24,8 +33,17 @@ public class Core {
         inGame = false;
         startFrame = new StartFrame(this);
     }
+
 	public void doMove(Move move) {
-		board.tryMove(move);
+        System.out.println(counter);
+        if (counter % 2 == 0) {
+            board.tryMove(move, player1);
+        }
+
+        if (counter % 2 == 1) {
+            board.tryMove(move, player2);
+        }
+        counter++;
 	}
     /*public static void startGame() {
         inGame = true;
