50c087f0755ae4614f536bbd5bc7b9a6d50c5a98
michaelyu15 <mxr15yu@gmail.com>
2020-04-21 17:14:23 -0400

    Created a ChatBox class to show output

---
+++ GUI/ChatBox.java
@@ -0,0 +1,70 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+class ChatBox extends JPanel {
+    //private JLabel systemOutput;
+    private JTextArea systemOutput;
+
+    public ChatBox() {
+        JLabel title = new JLabel("Chat Log");
+        systemOutput = new JTextArea(14,18);
+        JScrollPane scrollPane = new JScrollPane(systemOutput);
+        add(title, BorderLayout.NORTH);
+        add(scrollPane, BorderLayout.CENTER);
+        systemOutput.setCaretPosition(systemOutput.getDocument().getLength());
+        setPreferredSize(new Dimension(200,400));
+
+        JButton endGameBtn = new JButton("End Game");
+        endGameBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        add(endGameBtn, BorderLayout.SOUTH);
+    }
+
+    public void appendText(final String text) {
+        if (EventQueue.isDispatchThread()) {
+            systemOutput.setText(systemOutput.getText() + text);
+        } else {
+            EventQueue.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    appendText(text);
+                    systemOutput.setCaretPosition(systemOutput.getDocument().getLength());
+                }
+            });
+        }
+    }
+}
+
+class StreamIntake extends OutputStream {
+
+    private String string = "";
+    private ChatBox chat;
+    private PrintStream system;
+
+    public StreamIntake(ChatBox chat, PrintStream system) {
+        this.system = system;
+        this.chat = chat;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        char c = (char) b;
+        String value = Character.toString(c);
+        string += value;
+        if (value.equals("\n")) {
+            chat.appendText(string);
+            string = "";
+        }
+        system.print(c);
+    }
+}
